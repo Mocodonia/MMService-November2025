00788e657f74bdfaf197cb147f38a0688be68a8b
7Zeb <66693249+7zeb@users.noreply.github.com>
2026-02-13 08:36:05 -0600

    finished night stuff for the archive

--- night.js
+++ night.js
@@ -15,7 +15,7 @@ const MocodoniaTileLayer = L.TileLayer.extend({
     getTileUrl: function(coords) {
         // Final URL: Correctly uses negative x/y from the Simple CRS
         const cacheBuster = Date.now();
-        return `/MMService/night-tiles/${coords.x},${coords.y}.png?v=${cacheBuster}`;
+        return `/MMService-November2025/night-tiles/${coords.x},${coords.y}.png?v=${cacheBuster}`;
     }
 });
 
